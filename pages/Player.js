18b01ebf9b93e0fdd7efda1fea8773819ef271da
Austine Gomez <austine.gomez@yahoo.com>
2021-02-19 01:15:03 -0600

    files

---
+++ pages/Player.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactAudioPlayer from 'react-audio-player';
+
+export class Player extends Component {
+    render() {
+        return (
+            <div >
+                <ReactAudioPlayer
+                    src="mix.mp3"
+                    controls
+                />
+            </div>
+        )
+    }
+}
+
+export default Player
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'react-jinke-music-player/assets/index.css'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Player from './Player';
+import Image from 'next/image';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+import { Button } from "shards-react";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      <Image src ={"/DJ.jpg"}         
+          alt="Picture of the author"
+          width={500}
+          height={500}/>
+        <Player />
+        <a href={'/mix.mp3'} download="DJHIC/DJFelon Free Style Mix Vol.1">  
+      <Button theme="success"  size="lg">DOWNLOAD MP3</Button>
+      </a>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Agdev 
+        </a>
+      </footer>
+    </div>
+  )
+}
